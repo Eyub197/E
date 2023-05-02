b4fb925bc6be60884ae42ac5e0b34f1bb55a8ed7
Eyub197 <78847161+Eyub197@users.noreply.github.com>
2023-05-02 20:07:08 +0300

    Add files via upload

---
+++ SkateboardsProjectNewAlmostReady/Program.cs
@@ -0,0 +1,32 @@
+﻿using SkateboardsProject.Presentation;
+using SkateboardsProject.Presentation.Display;
+using System;
+
+namespace SkateboardsProject
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                Display display = new Display();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Error :(");
+            }
+            finally
+            {
+                Console.WriteLine("Have a nice day");
+            }
+            //control + . reabane
+            //To do 
+            //Fix the list methods so its Displays it like a table
+            //Make a better update method
+            //comments and hml
+            //Add to the methods like in the update after the update "The table name has been updateted"
+            //like that for delete, add, and whater else
+        }
+    }
+}
