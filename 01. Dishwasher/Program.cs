1b3cdf26f4345a4e89b4ded374afc6c6b7bdff60
Eyub <78847161+Eyub197@users.noreply.github.com>
2024-01-26 10:31:15 +0200

    Add files via upload

---
+++ 01. Dishwasher/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.IO;
+using System.Linq.Expressions;
+
+namespace _01._Dishwasher
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int bottels = int.Parse(Console.ReadLine());
+            double preparat = bottels * 750;
+            string input;
+            int caounter = 0;
+            double caountOfPlates = 0;
+            double caountOfPods = 0; ;
+            double neededPreparat = 0;
+            int caountOfPlatsOrpots = 0;
+            
+           
+
+            while ((input = Console.ReadLine()) != "End")
+            {
+                 caountOfPlatsOrpots = int.Parse(input);
+
+                if (caounter == 2)
+                {
+                    double podsNeeds = caountOfPlatsOrpots * 15;
+                    caounter = 0;
+                    neededPreparat = preparat - (preparat - podsNeeds);
+                    preparat = preparat - neededPreparat;
+                    caountOfPods += caountOfPlatsOrpots;
+                }
+                else
+                {
+                    double plateNeeds = caountOfPlatsOrpots * 5;
+                    neededPreparat = preparat - (preparat - plateNeeds);
+                    preparat = preparat - neededPreparat;
+                    caounter++;
+                    caountOfPlates += caountOfPlatsOrpots;
+                }
+                 
+
+                if (preparat < 0)
+                {
+                    break;
+                }
+            }
+
+            if (input == "End")
+            {
+                Console.WriteLine($"Detergent was enough!");
+                Console.WriteLine($"{caountOfPlates} dishes and {caountOfPods} pots were washed.");
+                Console.WriteLine($"Leftover detergent {preparat} ml.");
+            }
+            else
+            {
+                Console.WriteLine($"Not enough detergent, {Math.Abs(preparat)} ml. more necessary!");
+            }
+
+        }
+    }
+}
---
+++ 02. Report System/Program.cs
@@ -0,0 +1,74 @@
+﻿using System;
+
+namespace _02._Report_System
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double expectedSum = double.Parse(Console.ReadLine());
+            string input;
+            double transaction;
+            int transactionCaount = 0;
+            double moneyWithCard = 0;
+            double moneyWithCash = 0;
+            double totalSum = 0;
+            double cardPayers = 0;
+            double cashPayers = 0;
+            while ((input = Console.ReadLine()) != "End")
+            {
+                if (totalSum >= expectedSum)
+                {
+                    break;
+                }
+
+                transaction = double.Parse(input);
+                transactionCaount++;
+
+                if (transactionCaount % 2 == 0 )
+                {
+                   
+                    
+                    if (transaction > 100 || transaction >= 10)
+                    {
+                        cardPayers++;
+                        moneyWithCard += transaction;
+                        totalSum += transaction;
+                        Console.WriteLine("Product sold!");
+                    }
+                    else
+                    {
+                        Console.WriteLine("Error in transaction!");   
+                    }
+                }
+                else 
+                {
+                    if (transaction <= 100)
+                    {
+                        cashPayers++;
+                        moneyWithCash += transaction;
+                        totalSum += transaction;
+                        Console.WriteLine("Product sold!");
+                    }
+                    else
+                    {
+                        Console.WriteLine("Error in transaction!");
+                    }
+                }
+            }
+
+            if (totalSum >= expectedSum)
+            {
+                Console.WriteLine($"Average CS: {moneyWithCash / cashPayers:f2}");
+                Console.WriteLine($"Average CC: {moneyWithCard / cardPayers:f2}");
+            }
+            else
+            {
+                Console.WriteLine("Failed to collect required money for charity.");
+            }
+
+        }
+    }
+ }
+    
+
